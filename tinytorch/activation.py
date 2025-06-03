ac2438e9b0495a343e0695ec7aea9d51b8a6fe2e
almeidava93 <88211872+almeidava93@users.noreply.github.com>
2025-06-03 13:58:56 -0300

    added missing parent class init

--- tinytorch/activation.py
+++ tinytorch/activation.py
@@ -2,6 +2,9 @@ from tinytorch.module import Module
 from tinytorch.tensor import Tensor
 
 class ReLU(Module):
+  def __init__(self):
+    super().__init__()
+    
   def forward(self, input: Tensor) -> Tensor:
     output = input * (input.value > 0)
     return output
