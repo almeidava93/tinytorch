9053d117e896dd4ddda852e77fedfeef5c353b4e
almeidava93 <88211872+almeidava93@users.noreply.github.com>
2025-06-03 14:00:24 -0300

    added missing parent cls init, added retain grads attr

--- tinytorch/parameter.py
+++ tinytorch/parameter.py
@@ -1,8 +1,10 @@
+import numpy as np
 from tinytorch.tensor import Tensor
 
 class Parameter(Tensor):
-  def __init__(self, value, _children=(), _op='', label=None, requires_grad: bool = True, *args, **kwargs):
-    super().__init__(value, _children, _op, label, requires_grad, *args, **kwargs)
+  def __init__(self, *args, **kwargs):
+    super().__init__( *args, **kwargs)
+    self.retain_grads = True
 
   def __repr__(self):
     return f'Parameter(value={self.value}{", label="+self.label if self.label else ""}{", op="+self._op if self._op else ""}{", requires_grad="+str(self.requires_grad)})'
