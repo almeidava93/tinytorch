0afb0f502239a7ddece5fa3c420d9b29c440d710
almeidava93 <88211872+almeidava93@users.noreply.github.com>
2025-06-02 22:55:39 -0300

    Added Parameter object to Linear Module

--- tinytorch/nn.py
+++ tinytorch/nn.py
@@ -1,5 +1,6 @@
 import numpy as np
 from tinytorch.module import Module
+from tinytorch.parameter import Parameter
 from tinytorch.tensor import Tensor
 
 
@@ -7,18 +8,20 @@ class Linear(Module):
   def __init__(self, in_features: int, out_features: int, bias: bool = True):
     self.in_features = in_features
     self.out_features = out_features
-    self.weight: Tensor = None
-    self.bias: Tensor = None
+    self.weight: Parameter = None
+    self.bias: Parameter = None
     self.use_bias: bool = bias
+
+    self.initialize_weights()
 
   def __repr__(self):
     return f'{self.__class__.__name__}(in_features={self.in_features}, out_features={self.out_features}, bias={self.use_bias}),'
 
   def initialize_weights(self, method: str = 'random'):
     if method == 'random':
-      self.weight = Tensor(np.random.randn(self.out_features, self.in_features))
+      self.weight = Parameter(np.random.randn(self.out_features, self.in_features))
       if self.use_bias:
-        self.bias = Tensor(np.random.randn(self.out_features, 1))
+        self.bias = Parameter(np.random.randn(self.out_features, 1))
     else:
       raise NotImplementedError
 
