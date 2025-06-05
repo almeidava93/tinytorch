65842a78f2d5e293d14bdac7d8314b53660ac7dc
almeidava93 <88211872+almeidava93@users.noreply.github.com>
2025-06-05 00:15:32 -0300

    fixed init weights method in Linear Module

--- tinytorch/nn.py
+++ tinytorch/nn.py
@@ -1,11 +1,13 @@
+from typing import Optional
 import numpy as np
 from tinytorch.module import Module
 from tinytorch.parameter import Parameter
 from tinytorch.tensor import Tensor
+import tinytorch
 import tinytorch.init
 
 class Linear(Module):
-  def __init__(self, in_features: int, out_features: int, bias: bool = True):
+  def __init__(self, in_features: int, out_features: int, bias: bool = True, init_method: Optional[str] = None, init_method_kwargs: dict = {}):
     super().__init__()
     self.in_features = in_features
     self.out_features = out_features
@@ -13,15 +15,17 @@ class Linear(Module):
     self.bias: Parameter = None
     self.use_bias: bool = bias
 
-    self.initialize_weights()
+    self.initialize_weights(init_method, **init_method_kwargs)
 
   def __repr__(self):
     return f'{self.__class__.__name__}(in_features={self.in_features}, out_features={self.out_features}, bias={self.use_bias})'
 
-  def initialize_weights(self, method: str = 'uniform', *args, **kwargs):
-    self.weight = getattr(tinytorch.init, method)(size=(self.in_features, self.out_features), *args, **kwargs)
+  def initialize_weights(self, method: Optional[str] = None, **kwargs):
+    if method is None:
+      method, init_method_kwargs = tinytorch.get_init_method()
+    self.weight = getattr(tinytorch.init, method)(size=(self.in_features, self.out_features), **init_method_kwargs, **kwargs)
     if self.use_bias:
-      self.bias = getattr(tinytorch.init, method)(size=(1, self.out_features), *args, **kwargs)
+      self.bias = getattr(tinytorch.init, method)(size=(1, self.out_features), **init_method_kwargs, **kwargs)
 
   def forward(self, input: Tensor):
     output = input @ self.weight + self.bias
